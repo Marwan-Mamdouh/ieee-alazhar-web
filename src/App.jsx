ddeb124e656c41e7e7b781d6fb7db73c64db8236
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-05-30 03:52:02 +0300

    refactor: remove the footer import and use in every page and add it one time only in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./component/Footer";
 import Home from "./Home";
 import About from "./About";
 import Events from "./Events";
 import EventDetails from "./EventDetails.jsx";
 import Committees from "./Committees";
 import Board from "./Board";
-import ContactUs from "./ContactUs"
+import ContactUs from "./ContactUs";
 import JoinUs from "./JoinUs";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <ScrollToTop />
-      {/* <Section />  */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/committees" element={<Committees />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="/eventdetails/:id" element={<EventDetails />} />{" "}
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/joinus" element={<JoinUs />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<ScrollToTop />
+			{/* <Section />  */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/events" element={<Events />} />
+				<Route path="/committees" element={<Committees />} />
+				<Route path="/board" element={<Board />} />
+				<Route path="/eventdetails/:id" element={<EventDetails />} />{" "}
+				<Route path="/contactus" element={<ContactUs />} />
+				<Route path="/joinus" element={<JoinUs />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/EventDetails.jsx
+++ src/EventDetails.jsx
@@ -1,4 +1,3 @@
-import Footer from "./component/Footer";
 import { useParams } from "react-router-dom";
 import aiGame from "../src/assets/events-img/AI Game.jpg";
 import codeBaker from "../src/assets/events-img/Code Baker.jpg";
@@ -51,252 +50,249 @@ import memory4 from "./assets/Event-details/TheSemicolonShow/memry4.jpeg";
 import memory5 from "./assets/Event-details/TheSemicolonShow/mmmmmm1.jpeg";
 import memory6 from "./assets/Event-details/TheSemicolonShow/mmmmmm6.jpeg";
 
-
 const EventDetails = () => {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const events = [
-    {
-      id: "1",
-      title: "What’s Ai Game",
-      description:
-        "The event explores how AI drives business growth and integrates with robotics and IoT. Attendees gain practical steps to start a career in AI and discover its diverse applications. A panel with AI engineers offers an interactive platform for discussions, addressing questions and enhancing understanding of AI’s industry impact.",
-      bannerColor: "bg-red-600",
-      bannerImage: aiGame,
-      speakers: [
-        {
-          title: "Mohammed Al Assal",
-          name: "Mohammed Al Assal",
-          imageSrc: alassal,
-        },
-        {
-          title: "AhmedHafez",
-          name: "Ahmed Hafez",
-          imageSrc: AhmedHafez,
-        },
-        {
-          title: "Ahmed Abo AlFotoh",
-          name: "Ahmed Abo AlFotoh",
-          imageSrc: aboAlfotoh,
-        },
-        {
-          title: "AhmedIbrahim",
-          name: "Ahmed Ibrahim",
-          imageSrc: AhmedIbrahim,
-        },
-        {
-          title: "Ali Ahmed",
-          name: "Amr Helal",
-          imageSrc: AmrHelal,
-        },
-      ],
-      memories: [
-        { imageSrc: mem1 },
-        { imageSrc: mem2 },
-        { imageSrc: mem3 },
-        { imageSrc: mem4 },
-        { imageSrc: mem5 },
-      ],
-    },
-    // code baker
-    {
-      id: "2",
-      title: "What’s Code Baker",
-      description:
-        "Held over 1.5 to 2 months, the camp covers multiple tracks, like Java, Data Science, Embedded Systems, Graphic Design,  HR, and etc. It provides hands-on learning experiences, helping participants build practical skills. Instructors are former trainees who have gained knowledge throughout the season, making the event a culmination of their learning journey.",
-      bannerColor: "bg-red-600",
-      bannerImage: codeBaker,
-      // speakers: [
-      //   {
-      //     title: "Dr. Sarah Mohamed",
-      //     imageSrc: Semicolon,
-      //   },
-      // ],
-      memories: [
-        { imageSrc: meeem1 },
-        { imageSrc: meeem2 },
-        { imageSrc: meeem3 },
-      ],
-    },
-    {
-      id: "3",
-      title: "What’s Junior Camp",
-      description:
-        "Held during the mid-year vacation for two weeks, the camp offers various programming tracks such as Scratch, Python, TYP, and Arduino, helping young learners develop coding skills. In its eighth edition, new media tracks like motion graphics and graphic design were introduced, expanding the learning experience beyond programming.",
-      bannerColor: "bg-red-600",
-      bannerImage: juniorCamp,
-      // speakers: [
-      //   {
-      //     title: "Dr. Sarah Mohamed",
-      //     imageSrc: Semicolon,
-      //   },
-      // ],
-      memories: [
-        { imageSrc: meem1 },
-        { imageSrc: meem2 },
-        { imageSrc: meem3 },
-        { imageSrc: meem4 },
-        { imageSrc: meem5 },
-        { imageSrc: meem6 },
-        { imageSrc: meem7 },
-        { imageSrc: meem8 },
-        { imageSrc: meem9 },
-      ],
-    },
-    {
-      id: "4",
-      title: "What’s KickStart",
-      description:
-        "The event spanned three days, covering computer science fundamentals on the first day, programming principles using Python on the second, and career roadmaps in fields like Data Science, Cybersecurity, AI, and Frontend on the third. It provided participants with the opportunity to explore different tech domains and take their first steps toward a career in technology.",
-      bannerColor: "bg-red-600",
-      bannerImage: kickStart,
-      // speakers: [
-      //   {
-      //     title: "Dr. Sarah Mohamed",
-      //     imageSrc: Semicolon,
-      //   },
-      // ],
-      memories: [
-        { imageSrc: mem_1 },
-        { imageSrc: mem_2 },
-        { imageSrc: mem_3 },
-        { imageSrc: mem_4 },
-        { imageSrc: mem_5 },
-      ],
-    },
-    {
-      id: "5",
-      title: "What’s The Semicolon Show",
-      description:
-        "Event Semicolon was held in two versions, one online on Codeforces and one offline. Participants formed teams of 3 members, with prizes awarded to the top 3 teams. The competition aims to strengthen problem-solving abilities and serves as preparation for global contests like IEEE Extreme and ICPC.",
-      bannerColor: "bg-red-600",
-      bannerImage: Semicolon,
-      speakers: [
-        {
-          title: "AbdallaMansour",
-          name: "Abdalla Mansour",
-          imageSrc: AbdallaMansour,
-        },
-        {
-          title: "MahmoudAlGezawy",
-          name: "Mahmoud AlGezawy",
-          imageSrc: MahmoudAlGezawy,
-        },
-        {
-          title: "mohamed Amr",
-          name: "Mohamed Amr",
-          imageSrc: mohamedAmr,
-        },
-        {
-          title: "MuhammadAlBahnasi",
-          name: "Muhammad AlBahnasi",
-          imageSrc: MuhammadAlBahnasi,
-        },
-        {
-          title: "TarekAlAbd",
-          name: "Tarek Al Abd",
-          imageSrc: tarekAlAbd,
-        },
-      ],
-      memories: [
-        { imageSrc: memory1 },
-        { imageSrc: memory2 },
-        { imageSrc: memory3 },
-        { imageSrc: memory4 },
-        { imageSrc: memory5 },
-        { imageSrc: memory6 },
-      ],
-    },
-  ];
+	const events = [
+		{
+			id: "1",
+			title: "What's Ai Game",
+			description:
+				"The event explores how AI drives business growth and integrates with robotics and IoT. Attendees gain practical steps to start a career in AI and discover its diverse applications. A panel with AI engineers offers an interactive platform for discussions, addressing questions and enhancing understanding of AI's industry impact.",
+			bannerColor: "bg-red-600",
+			bannerImage: aiGame,
+			speakers: [
+				{
+					title: "Mohammed Al Assal",
+					name: "Mohammed Al Assal",
+					imageSrc: alassal,
+				},
+				{
+					title: "AhmedHafez",
+					name: "Ahmed Hafez",
+					imageSrc: AhmedHafez,
+				},
+				{
+					title: "Ahmed Abo AlFotoh",
+					name: "Ahmed Abo AlFotoh",
+					imageSrc: aboAlfotoh,
+				},
+				{
+					title: "AhmedIbrahim",
+					name: "Ahmed Ibrahim",
+					imageSrc: AhmedIbrahim,
+				},
+				{
+					title: "Ali Ahmed",
+					name: "Amr Helal",
+					imageSrc: AmrHelal,
+				},
+			],
+			memories: [
+				{ imageSrc: mem1 },
+				{ imageSrc: mem2 },
+				{ imageSrc: mem3 },
+				{ imageSrc: mem4 },
+				{ imageSrc: mem5 },
+			],
+		},
+		// code baker
+		{
+			id: "2",
+			title: "What’s Code Baker",
+			description:
+				"Held over 1.5 to 2 months, the camp covers multiple tracks, like Java, Data Science, Embedded Systems, Graphic Design,  HR, and etc. It provides hands-on learning experiences, helping participants build practical skills. Instructors are former trainees who have gained knowledge throughout the season, making the event a culmination of their learning journey.",
+			bannerColor: "bg-red-600",
+			bannerImage: codeBaker,
+			// speakers: [
+			//   {
+			//     title: "Dr. Sarah Mohamed",
+			//     imageSrc: Semicolon,
+			//   },
+			// ],
+			memories: [
+				{ imageSrc: meeem1 },
+				{ imageSrc: meeem2 },
+				{ imageSrc: meeem3 },
+			],
+		},
+		{
+			id: "3",
+			title: "What’s Junior Camp",
+			description:
+				"Held during the mid-year vacation for two weeks, the camp offers various programming tracks such as Scratch, Python, TYP, and Arduino, helping young learners develop coding skills. In its eighth edition, new media tracks like motion graphics and graphic design were introduced, expanding the learning experience beyond programming.",
+			bannerColor: "bg-red-600",
+			bannerImage: juniorCamp,
+			// speakers: [
+			//   {
+			//     title: "Dr. Sarah Mohamed",
+			//     imageSrc: Semicolon,
+			//   },
+			// ],
+			memories: [
+				{ imageSrc: meem1 },
+				{ imageSrc: meem2 },
+				{ imageSrc: meem3 },
+				{ imageSrc: meem4 },
+				{ imageSrc: meem5 },
+				{ imageSrc: meem6 },
+				{ imageSrc: meem7 },
+				{ imageSrc: meem8 },
+				{ imageSrc: meem9 },
+			],
+		},
+		{
+			id: "4",
+			title: "What’s KickStart",
+			description:
+				"The event spanned three days, covering computer science fundamentals on the first day, programming principles using Python on the second, and career roadmaps in fields like Data Science, Cybersecurity, AI, and Frontend on the third. It provided participants with the opportunity to explore different tech domains and take their first steps toward a career in technology.",
+			bannerColor: "bg-red-600",
+			bannerImage: kickStart,
+			// speakers: [
+			//   {
+			//     title: "Dr. Sarah Mohamed",
+			//     imageSrc: Semicolon,
+			//   },
+			// ],
+			memories: [
+				{ imageSrc: mem_1 },
+				{ imageSrc: mem_2 },
+				{ imageSrc: mem_3 },
+				{ imageSrc: mem_4 },
+				{ imageSrc: mem_5 },
+			],
+		},
+		{
+			id: "5",
+			title: "What’s The Semicolon Show",
+			description:
+				"Event Semicolon was held in two versions, one online on Codeforces and one offline. Participants formed teams of 3 members, with prizes awarded to the top 3 teams. The competition aims to strengthen problem-solving abilities and serves as preparation for global contests like IEEE Extreme and ICPC.",
+			bannerColor: "bg-red-600",
+			bannerImage: Semicolon,
+			speakers: [
+				{
+					title: "AbdallaMansour",
+					name: "Abdalla Mansour",
+					imageSrc: AbdallaMansour,
+				},
+				{
+					title: "MahmoudAlGezawy",
+					name: "Mahmoud AlGezawy",
+					imageSrc: MahmoudAlGezawy,
+				},
+				{
+					title: "mohamed Amr",
+					name: "Mohamed Amr",
+					imageSrc: mohamedAmr,
+				},
+				{
+					title: "MuhammadAlBahnasi",
+					name: "Muhammad AlBahnasi",
+					imageSrc: MuhammadAlBahnasi,
+				},
+				{
+					title: "TarekAlAbd",
+					name: "Tarek Al Abd",
+					imageSrc: tarekAlAbd,
+				},
+			],
+			memories: [
+				{ imageSrc: memory1 },
+				{ imageSrc: memory2 },
+				{ imageSrc: memory3 },
+				{ imageSrc: memory4 },
+				{ imageSrc: memory5 },
+				{ imageSrc: memory6 },
+			],
+		},
+	];
 
-  const event = events.find((e) => e.id === id);
+	const event = events.find((e) => e.id === id);
 
-  if (!event) {
-    return (
-      <div className="p-6 text-center">
-        <h1 className="text-xl font-bold">Event Not Found</h1>
-      </div>
-    );
-  }
+	if (!event) {
+		return (
+			<div className="p-6 text-center">
+				<h1 className="text-xl font-bold">Event Not Found</h1>
+			</div>
+		);
+	}
 
-  return (
-    <div>
-      <section
-        className={`relative h-[500px] w-full bg-center bg-cover ${
-          !event.bannerImage ? event.bannerColor : ""
-        }`}
-        style={
-          event.bannerImage
-            ? { backgroundImage: `url(${event.bannerImage})` }
-            : {}
-        }
-      />
+	return (
+		<div>
+			<section
+				className={`relative h-[500px] w-full bg-center bg-cover ${
+					event.bannerImage ? "" : event.bannerColor
+				}`}
+				style={
+					event.bannerImage
+						? { backgroundImage: `url(${event.bannerImage})` }
+						: {}
+				}
+			/>
 
-      <div className="w-fit px-4 py-6 relative top-[-45px] bg-white m-auto rounded-2xl shadow-md mx-3 sm:mx-5 md:mx-10 lg:mx-20">
-        <h2 className="flex flex-col text-center gap-2">
-          <span
-            className={`w-fit ${event.bannerColor} text-white px-3 py-1 rounded-tr-2xl rounded-br-2xl text-base sm:text-lg md:text-xl font-bold`}
-          >
-            {event.title}
-          </span>
-          <span className="text-[#1A1A1A] text-sm sm:text-base md:text-lg font-semibold">
-            {event.description}
-          </span>
-        </h2>
-      </div>
+			<div className="w-fit px-4 py-6 relative top-[-45px] bg-white m-auto rounded-2xl shadow-md mx-3 sm:mx-5 md:mx-10 lg:mx-20">
+				<h2 className="flex flex-col text-center gap-2">
+					<span
+						className={`w-fit ${event.bannerColor} text-white px-3 py-1 rounded-tr-2xl rounded-br-2xl text-base sm:text-lg md:text-xl font-bold`}
+					>
+						{event.title}
+					</span>
+					<span className="text-[#1A1A1A] text-sm sm:text-base md:text-lg font-semibold">
+						{event.description}
+					</span>
+				</h2>
+			</div>
 
-      {event.speakers && event.speakers.length > 0 && (
-        <div className="my-10 px-6">
-          <h2 className="flex flex-col sm:flex-row items-start sm:items-center text-lg sm:text-2xl font-bold gap-2 mb-8">
-            <span className="bg-red-600 text-white px-2 py-1 rounded-tr-2xl rounded-br-2xl">
-              Our Speaker
-            </span>
-            <span className="text-[#1A1A1A]">
-              Gain valuable knowledge from the brightest minds in tech and
-              engineering.
-            </span>
-          </h2>{" "}
-          <CardSlider
-            cards={event.speakers.map((speaker, index) => (
-              <Card
-                key={index}
-                title={speaker.title}
-                name={speaker.name}
-                imageSrc={speaker.imageSrc}
-                showLinkedin={false}
-              />
-            ))}
-          />
-        </div>
-      )}
-      {event.memories && event.memories.length > 0 && (
-        <div className="my-10 px-6">
-          <h2 className="flex flex-col sm:flex-row items-start sm:items-center text-lg sm:text-2xl font-bold gap-2 mb-8">
-            <span className="bg-red-600 text-white px-2 py-1 rounded-tr-2xl rounded-br-2xl">
-              Moments That Inspire
-            </span>
-            <span className="text-[#1A1A1A]">
-              Capturing the highlights of our innovative and collaborative
-              events.
-            </span>
-          </h2>
+			{event.speakers && event.speakers.length > 0 && (
+				<div className="my-10 px-6">
+					<h2 className="flex flex-col sm:flex-row items-start sm:items-center text-lg sm:text-2xl font-bold gap-2 mb-8">
+						<span className="bg-red-600 text-white px-2 py-1 rounded-tr-2xl rounded-br-2xl">
+							Our Speaker
+						</span>
+						<span className="text-[#1A1A1A]">
+							Gain valuable knowledge from the brightest minds in tech and
+							engineering.
+						</span>
+					</h2>{" "}
+					<CardSlider
+						cards={event.speakers.map((speaker, index) => (
+							<Card
+								key={`${index}-${speaker.title}`}
+								title={speaker.title}
+								name={speaker.name}
+								imageSrc={speaker.imageSrc}
+								showLinkedin={false}
+							/>
+						))}
+					/>
+				</div>
+			)}
+			{event.memories && event.memories.length > 0 && (
+				<div className="my-10 px-6">
+					<h2 className="flex flex-col sm:flex-row items-start sm:items-center text-lg sm:text-2xl font-bold gap-2 mb-8">
+						<span className="bg-red-600 text-white px-2 py-1 rounded-tr-2xl rounded-br-2xl">
+							Moments That Inspire
+						</span>
+						<span className="text-[#1A1A1A]">
+							Capturing the highlights of our innovative and collaborative
+							events.
+						</span>
+					</h2>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {event.memories.map((memory, index) => (
-              <img
-                key={index}
-                src={memory.imageSrc}
-                alt={`Memory ${index + 1}`}
-                className="w-full h-64 object-cover rounded-lg shadow-md"
-              />
-            ))}
-          </div>
-        </div>
-      )}
-
-      <Footer />
-    </div>
-  );
+					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+						{event.memories.map((memory, index) => (
+							<img
+								key={`${index}-${memory.imageSrc}`}
+								src={memory.imageSrc}
+								alt={`Memory ${index + 1}`}
+								className="w-full h-64 object-cover rounded-lg shadow-md"
+							/>
+						))}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default EventDetails;

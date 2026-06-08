5c6d021934099891ad6ee094403813c36329a234
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-08 04:14:01 +0300

    refacotr: remove some exports as we do not need to imports these interfaces from the hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useFetch } from "./useFetch/useFetch";
-export type { UseFetchOptions, UseFetchResult } from "./useFetch/types";
 
 export { default as useScrollToTop } from "./useScrollToTop/useScrollToTop";
